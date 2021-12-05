2e6b1a96b1bb1794e06a56bd75168b472e6b1a14
emon hasan <emonhasan1010@gmail.com>
2021-12-05 20:42:08 +0600

    Natural Language Processing Dataset & Code

---
+++ Natural_Language_Processing/natural_language_processing.py
@@ -0,0 +1,13 @@
+# Natural Language Processing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Restaurant_Reviews.tsv',delimiter='\t',quoting=3)
+
+#Cleaning the texts
+import re
+review = re.sub('[^a-zA-Z]',' ',dataset['Review'][0])
