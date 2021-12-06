9cdc66e73dc9df36a4722314ca542006013c1c46
emon hasan <emonhasan1010@gmail.com>
2021-12-06 15:17:36 +0600

    Updated NLP code

--- Natural_Language_Processing/natural_language_processing.py
+++ Natural_Language_Processing/natural_language_processing.py
@@ -10,4 +10,38 @@ dataset = pd.read_csv('Restaurant_Reviews.tsv',delimiter='\t',quoting=3)
 
 #Cleaning the texts
 import re
-review = re.sub('[^a-zA-Z]',' ',dataset['Review'][0])
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+corpus = []
+for i in range(0,1000):
+    review = re.sub('[^a-zA-Z]',' ',dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+
+#Creating the Bag of Words model
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features=1500)
+X = cv.fit_transform(corpus).toarray()
+Y = dataset.iloc[:,1].values
+
+#splitting the dataset into Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.20,random_state=0)
+
+#fitting Naive Bayes to the training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train,Y_train)
+
+#predicting the test set results
+Y_pred = classifier.predict(X_test)
+
+#making the confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(Y_test,Y_pred)
